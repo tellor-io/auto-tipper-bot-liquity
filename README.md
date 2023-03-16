ac128ce4f0e600bdd32b8307c7ff4ff823ce5048
tkernell <tpkernell@gmail.com>
2023-03-16 14:34:14 -0500

    check trb balance

--- README.md
+++ README.md
@@ -37,8 +37,24 @@ tipper
 ### How It Works
 The bot will automatically determine a tip amount as a function of the current gas cost, oracle token price (TRB), and base token price (ETH, MATIC). It calculates the cost of paying for gas in terms of the oracle token and adds a buffer of $2. It then waits 45 seconds for a data report. If a report is submitted, the bot then waits for the next tipping interval. If no report was submitted, the bot recalculates the gas cost in terms of the oracle token, adds the $2 buffer, and multiplies this value by 1.10. It keeps doing this until a report is submitted or up to a max of 10 times.
 
+<!-- <img src="./assets/tip-graph1.png" width="500" class="center"> -->
+<p align="center">
+  <img src="./assets/tip-graph1.png" width="500">
+</p>
+
 ### Why Use This
 The Tellor oracle works by incentivizing data reporters to submit your requested data. Reporters have to cover gas costs plus earn some profit. This bot is a handy tool for anyone who wants to make sure their data is reported at a fixed interval while accounting for varying gas costs. When gas costs are low, the bot will tip a lower amount. When gas costs are high, the bot will tip a higher amount.
+
+### To Do
+- [x] update contract addresses to latest
+- [ ] check base token and tipping token balances
+- [ ] create generalized contract function caller
+- [ ] retrieve token prices from multiple sources, handle errors, and medianize
+- [ ] add logging to file with garbage collection
+- [ ] allow multiple simultaneous queryId's with different tipping intervals
+- [ ] dynamic query types, ie. TellorRNG(timestamp) with different timestamp each time -> different queryId
+- [ ] comment code, documentation, and tests
+- [ ] notifications (text/email/discord/telegram) configurable by user (tips, reports, low balance, etc.)
 
 ## Maintainers <a name="maintainers"> </a>
 This repository is maintained by the [Tellor team](https://github.com/orgs/tellor-io/people)
--- tipper_bot/simple_tipper.py
+++ tipper_bot/simple_tipper.py
@@ -70,6 +70,8 @@ def get_gas_cost_in_oracle_token():
         print("gas price: ", gas_price)
 
         # convert gas cost to oracle token: gas_price * gas_cost * base_token_price / oracle_price
+        gas_cost_usd = config.total_gas_cost * gas_price * base_token_price / 1000000000
+        print("gas cost in usd: ", gas_cost_usd)
         gas_cost_oracle_token = gas_price * config.total_gas_cost * \
             base_token_price / oracle_token_price / 1000000000
         return (gas_cost_oracle_token, trb_price)
@@ -96,10 +98,11 @@ def get_required_tip(try_count):
 
     # handle api errors with limited retries
     api_max_tries = config.api_max_tries
-    api_try_count = 1
+    api_try_count = 0
     while trb_price == 0.0 and api_try_count < api_max_tries:
-        print("trb price is 0, trying again in", 5 * api_try_count, "seconds")
-        time.sleep(5 * api_try_count)
+        sleep_time = 5 * 2 ** api_try_count
+        print("trb price is 0, trying again in", sleep_time, "seconds")
+        time.sleep(sleep_time)
         (gas_cost_trb, trb_price) = get_gas_cost_in_oracle_token()
         api_try_count += 1
 
@@ -117,7 +120,7 @@ def get_required_tip(try_count):
                     trb_price) * tip_multiplier ** try_count
     return required_tip
 
-
+# function for getting last report time for given query id
 def get_last_report_time(query_id):
     current_time = datetime.datetime.now()
     print("current time: ", current_time.timestamp())
@@ -134,7 +137,6 @@ def get_last_report_time(query_id):
 
 def tip(amount_to_tip, query_id, query_data):
     print("tipping: ", amount_to_tip)
-    print("here")
     # build transaction
 
     try:
@@ -170,7 +172,9 @@ def tip(amount_to_tip, query_id, query_data):
     web3.eth.waitForTransactionReceipt(tx_hash)
 
 
-def initiate_tipping_sequence(retip_count, query_id, query_data, last_report_time):
+
+
+def initiate_tipping_sequence(retip_count, query_id, query_data, last_report_time, balances):
     # calculate required tip
     required_tip = get_required_tip(retip_count)
     print("required tip: ", required_tip)
@@ -189,17 +193,30 @@ def initiate_tipping_sequence(retip_count, query_id, query_data, last_report_tim
         query_id, int(current_time.timestamp())).call()
     last_report_time_updated = int(data_before[2])
     print("last report time updated: ", last_report_time_updated)
-    print("last report time: ", last_report_time)
+    print("last report time:         ", last_report_time)
 
-    # check if current tip is less than required tip and last report time has not changed
+    # check if current tip is less than required tip or last report time has not changed
     amount_to_tip = 0
-    if current_tip < (required_tip * 1e18) and last_report_time_updated == last_report_time:
+    if current_tip < (required_tip * 1e18) or last_report_time_updated == last_report_time:
         # calculate tip amount
         amount_to_tip = (required_tip * 1e18) - current_tip
-        print("tip amount: ", amount_to_tip / 1e18)
+        if amount_to_tip < 0:
+            amount_to_tip = 0
+        print("amount to tip: ", amount_to_tip / 1e18)
 
-        # call tip function
-        tip(amount_to_tip, query_id, query_data)
+        # check if oracle token balance is zero
+        if(balances[0] == 0):
+            print("zero", config.oracle_token_price_url_selector, "oracle token balance, exiting")
+            return
+        # check if oracle token balance is less than amount to tip
+        if(balances[0] < amount_to_tip):
+            print("not enough", config.oracle_token_price_url_selector, "oracle token balance to tip")
+            print("using all", config.oracle_token_price_url_selector, "oracle token balance to tip")
+            amount_to_tip = balances[0]
+            print("new amount to tip: ", amount_to_tip / 1e18)
+        
+        if(amount_to_tip > 0):
+            tip(amount_to_tip, query_id, query_data)
 
         # wait 20 seconds
         print("sleeping...")
@@ -218,15 +235,16 @@ def initiate_tipping_sequence(retip_count, query_id, query_data, last_report_tim
             print("no new data reported")
             # check if try count is less than max try count
             if retip_count < max_retip_count:
+                balances = approve_token_and_check_balance()
                 # initiate tipping sequence again
                 print("try count ", retip_count,
                     " is less than max try count ", max_retip_count)
-                initiate_tipping_sequence(retip_count + 1, query_id, query_data, last_report_time)
+                initiate_tipping_sequence(retip_count + 1, query_id, query_data, last_report_time, balances)
         else:
             print("new data reported")
 
 
-def approve_token():
+def approve_token_and_check_balance():
     # check token allowance
     token_allowance = oracle_token_contract.functions.allowance(
         acct.address, autopay_contract.address).call()
@@ -257,6 +275,13 @@ def approve_token():
         print("Transaction hash: ", tx_hash)
         # wait for transaction to be mined
         web3.eth.wait_for_transaction_receipt(tx_hash)
+    oracle_token_balance = oracle_token_contract.functions.balanceOf(
+        acct.address).call()
+    print(config.oracle_token_price_url_selector, "token balance: ", oracle_token_balance / 1e18)
+    base_token_balance = web3.eth.get_balance(acct.address)
+    print(config.base_token_price_url_selector, "token balance: ", base_token_balance / 1e18)
+    return [oracle_token_balance, base_token_balance]
+
 
 
 def main():
@@ -265,17 +290,24 @@ def main():
     interval = config.interval
     start_time = config.start_time
 
-    approve_token()
+    balances = approve_token_and_check_balance()
     while True:
         last_report_time = get_last_report_time(query_id=query_id)
         seconds_until_next_interval = get_seconds_until_next_interval(interval=interval, start_time=start_time)
         current_timestamp = datetime.datetime.now().timestamp()
-        if seconds_until_next_interval < 60 and int(last_report_time) < int(current_timestamp) - int(interval / 10):
+        init_tip_bool = True
+        if balances[0] == 0:
+            print("zero", config.oracle_token_price_url_selector, "oracle token balance")
+            init_tip_bool = False
+        if balances[1] == 0:
+            print("zero", config.base_token_price_url_selector, "base token balance")
+            init_tip_bool = False
+        if seconds_until_next_interval < 60 and int(last_report_time) < int(current_timestamp) - int(interval / 10) and init_tip_bool:
             # initiate tipping sequence
             print("\ninitiating tipping sequence")
-            initiate_tipping_sequence(retip_count=0, query_id=query_id, query_data=query_data, last_report_time=last_report_time)
+            initiate_tipping_sequence(retip_count=0, query_id=query_id, query_data=query_data, last_report_time=last_report_time, balances=balances)
         else:
-            approve_token()
+            balances = approve_token_and_check_balance()
             # sleep until next interval
             print("sleeping until next interval")
             time.sleep(seconds_until_next_interval / 2)
